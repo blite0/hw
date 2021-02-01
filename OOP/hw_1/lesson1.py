a9383abd0e4534a786d764cb3880a519495da873
blite0 <Ravildos90@gmail.com>
2021-02-01 20:21:48 +0200

    hw_1 OOP

---
+++ OOP/hw_1/lesson1.py
@@ -0,0 +1,32 @@
+class Elements(object):
+    zamerzanya = 1537
+    plavlenya = 1538
+    isparenya = 2862
+    t = 0
+
+    def agg_state(self, t):
+        celsie = t
+        farengeit = (t * 9 / 5) + 32
+        celvin = t + 273, 15
+        if self.t >= self.plavlenya and self.t < self.isparenya:
+            print('plavimsya')
+        elif self.t < self.plavlenya:
+            print('Tverdeem')
+        else:
+            print('Mi isparyaemsya')
+
+        print('Цельсий =', celsie, 'Фаренгейт = ', farengeit, 'Кельвин = ', celvin)
+    def convertation(temperature):
+        celsie2 = temperature
+        farengeit2 = (temperature * 9 / 5) + 32
+        celvin2 = temperature + 273,15
+
+        print('Цельсий =', celsie2, 'Фаренгейт = ', farengeit2, 'Кельвин = ', celvin2)
+
+
+proba1 = Elements()
+proba1.t = int(input("Введите температуру металла:"))
+
+proba1.agg_state(proba1.t)
+class Iron(Elements):
+    pass
