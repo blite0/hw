d24c510055432c9c385c8bad811014f5c3d71a15
blite0 <Ravildos90@gmail.com>
2021-02-08 22:06:32 +0200

    hw3

---
+++ OOP/hw_3/app.py
@@ -0,0 +1,15 @@
+import models
+
+
+def main():
+    recruiter = models.Recruiter('Sam', 'Serioussam', 150)
+    programmer_phyton = models.Programmer('Hitman', 'hitmanovich@gmail.com', 200)
+    programmer_java = models.Programmer('Putin', 'putinvv@gmail.com', 1)
+    candidate_phyton = models.Candidate('Eric Davidovich', 'ericdavidovich@gmail.com', 'programming',
+                                        'phyton', 'junior')
+    candidate_phyton2 = models.Candidate('Vladimir Zelencskiy', 'vladimirgreen@gmail.com', 'phyton',
+                                         'programming', 'senior')
+    candidate_phyton3 = models.Candidate('Petr Poroshenko', 'roshen@gmail.com', 'phyton',
+                                         'programming', 'junior')
+    vacancy_first = models.Vacancy("Phyton", 'django', 'senior')
+    vacancy_second = models.Vacancy("Phyton", 'django', 'junior')
